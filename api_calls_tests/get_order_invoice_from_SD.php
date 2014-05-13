8abb953b721fd50c96b7c446abaf02c48d525b51
Jacek <jacek@ivolution.pl>
2014-05-14 01:06:38 +0200

    SD-54

--- api_calls_tests/get_order_invoice_from_SD.php
+++ api_calls_tests/get_order_invoice_from_SD.php
@@ -6,7 +6,7 @@ require_once(dirname(__FILE__).'/../includes/SureDone_Startup.php');
 $token = "452B9137D17FDF14482E87BB78FB1781B3A854CCF1399D43EF5983D44414FDA48162106218EF830C6J1ONVAVKW5LVCN9U8FQA023ULATCFNSRLYJGUOWA1MZON0XEX82MJDU4YXHXJPKURBP2HQBJCTGS10WTQ55OHZVFGLRQ480";
 $username = 'yd';
 /**
- * gets order invoice - order has to be shipped
+ * gets order invoice
  */
 try{
     $id = 'SD1310979759453';
