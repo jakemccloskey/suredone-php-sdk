bf6a7458312ffd7aa5d4b5e6b24f237b7b46c1c1
Jacek <jacek@ivolution.pl>
2014-05-14 14:36:16 +0200

    SD-58

--- api_calls_tests/update_order_in_SD.php
+++ api_calls_tests/update_order_in_SD.php
@@ -5,7 +5,10 @@ ini_set('display_errors', 1);
 require_once(dirname(__FILE__).'/../includes/SureDone_Startup.php');
 $token = "452B9137D17FDF14482E87BB78FB1781B3A854CCF1399D43EF5983D44414FDA48162106218EF830C6J1ONVAVKW5LVCN9U8FQA023ULATCFNSRLYJGUOWA1MZON0XEX82MJDU4YXHXJPKURBP2HQBJCTGS10WTQ55OHZVFGLRQ480";
 $username = 'yd';
-/**
+/** for fullfilment:
+* Standard codes: NOTRACKING|REFUNDED|CANCELED|PICKUP ( will not be read as tracking number )
+* Special codes: PENDING|ORDERED|SHIPPED|DROPSHIPPED ( will not sync or email customer )
+*
  * update order
  */
 try{
